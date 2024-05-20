4228ec6785d7d52111939cd7fa684ab1478350bf
PulsarProgrammer <programingcoder101@gmail.com>
2024-05-20 12:53:51 -0400

    Initial Commit!!!

---
+++ src/Player.java
@@ -0,0 +1,114 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public class Player extends Entity {
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler kh){
+        this.gp = gp;
+        keyH = kh;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues(){
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void update(){
+        if(keyH.upPressed){
+            direction = "up";
+            y -= speed;
+        }
+        if(keyH.downPressed){
+            direction = "down";
+            y += speed;
+        }
+        if(keyH.leftPressed){
+            direction = "left";
+            x -= speed;
+        }
+        if(keyH.rightPressed){
+            direction = "right";
+            x += speed;
+        }
+        if(keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed){
+            spriteCounter += 1;
+            if(spriteCounter > 10){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
+    }
+    public void draw(Graphics2D g2){
+        // g2.setColor(Color.white);
+        // g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+        switch(direction){
+            case "up": 
+                if(spriteNum == 1){
+                    image = up1;
+                }
+                if(spriteNum == 2){
+                    image = up2;
+                }
+                break;
+            case "left": 
+                if(spriteNum == 1){
+                    image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                }
+                break;
+            case "right": 
+                if(spriteNum == 1){
+                    image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                }
+                break;
+            case "down":
+            default: 
+                if(spriteNum == 1){
+                    image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+
+    public void getPlayerImage(){
+        try {
+            // String classpath = System.getProperty("java.class.path");
+            // System.out.println(classpath);
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_2.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
